8f8963518dd7fced9859dfaf2f6fe070f701a319
Vinicius Kawazoe <vinikawazoe@gmail.com>
2020-08-15 11:44:43 -0300

    Delete seller

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -7,23 +7,28 @@ using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Services;
 
-namespace SalesWebMVC.Controllers {
-    public class SellersController : Controller {
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
 
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService, DepartmentService departmentService) {
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
+        {
             _sellerService = sellerService;
             _departmentService = departmentService;
         }
 
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
             var list = _sellerService.FindAll();
             return View(list);
         }
 
-        public IActionResult Create() {
+        public IActionResult Create()
+        {
             var departments = _departmentService.FindAll();
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
@@ -31,8 +36,33 @@ namespace SalesWebMVC.Controllers {
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller) {
+        public IActionResult Create(Seller seller)
+        {
             _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
     }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -5,20 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SalesWebMVC.Services {
-    public class SellerService {
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
         private readonly SalesWebMVCContext _context;
 
-        public SellerService(SalesWebMVCContext context) {
+        public SellerService(SalesWebMVCContext context)
+        {
             _context = context;
         }
 
-        public List<Seller> FindAll() {
+        public List<Seller> FindAll()
+        {
             return _context.Seller.ToList();
         }
 
-        public void Insert(Seller obj) {
+        public void Insert(Seller obj)
+        {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
